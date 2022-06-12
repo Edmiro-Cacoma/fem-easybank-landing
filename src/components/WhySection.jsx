8e4451537ac2904c3e370beac434beae4f70a8f3
Elias Fung <manchai.fung@gmail.com>
2022-06-12 18:08:03 -0400

    5

--- src/components/WhySection.jsx
+++ src/components/WhySection.jsx
@@ -3,22 +3,22 @@ import React from "react";
 function WhySection() {
     const data = [
         {
-            image: "../../public/images/icon-online.svg",
+            image: "/images/icon-online.svg",
             title: "Online Banking",
             text: "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world."
         },
         {
-            image: "../../public/images/icon-budgeting.svg",
+            image: "/images/icon-budgeting.svg",
             title: "Simple Budgeting",
             text: "See exactly where your money goes each month. Receive notifications when you're close to hitting your limits."
         },
         {
-            image: "../../public/images/icon-onboarding.svg",
+            image: "/images/icon-onboarding.svg",
             title: "Fast Onboarding",
             text: "We don't do branches. Open your account in minutes online and start taking control of your finances right away."
         },
         {
-            image: "../../public/images/icon-api.svg",
+            image: "/images/icon-api.svg",
             title: "Open API",
             text: "Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier."
         }
